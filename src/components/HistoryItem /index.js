75b7488ec7c5ec66e550f6c620ed00556041dd28
siddharthcode98 <139576040+siddharthcode98@users.noreply.github.com>
2023-08-30 13:30:20 +0530

    Update index.js

--- src/components/HistoryItem /index.js
+++ src/components/HistoryItem /index.js
@@ -1,11 +1,15 @@
 import "./index.css";
 
 const HistoryItem = (props) => {
+
   const { eachHistory, deleteHistory } = props;
+
   const { id, timeAccessed, logoUrl, title, domainUrl } = eachHistory;
+  
   const deleteList = () => {
     deleteHistory(id);
   };
+  
   return (
     <li className="listStyle">
       <p>{timeAccessed}</p>
@@ -14,8 +18,9 @@ const HistoryItem = (props) => {
       </div>
       <p className="Title">{title}</p>
       <p className="DomainUrl">{domainUrl}</p>
-      <button data-testid="delete" onClick={deleteList}> <imgsrc="https://assets.ccbp.in/frontend/react-js/delete-img.png" alt="delete"/></button>
+      <button data-testid="delete" onClick={deleteList}> <img src="https://assets.ccbp.in/frontend/react-js/delete-img.png" alt="delete"/></button>
     </li>
   );
 };
+
 export default HistoryItem;
